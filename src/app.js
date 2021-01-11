f1c44050859698338423503ffe7810bc09fbbfd0
wuyl <2331336537@qq.com>
2021-01-11 19:08:35 +0800

    feat: 添加button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import Vue from 'vue/dist/vue.esm'
 import Button from './button'
 import Icon from './icon'
-
+import ButtonGroup from './button-group.vue'
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
+Vue.component('r-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
