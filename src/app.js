a5c71f75de5f3b1d28104f19bca97285066784aa
wuyl <2331336537@qq.com>
2021-01-20 22:28:44 +0800

    feat:简单实现row,col

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import Vue from 'vue/dist/vue.esm'
 import Button from './button'
 import Icon from './icon'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 import ButtonGroup from './button-group.vue'
 Vue.component('r-button', Button)
 Vue.component('r-icon', Icon)
 Vue.component('r-input', Input)
+Vue.component('r-row', Row)
+Vue.component('r-col', Col)
 Vue.component('r-button-group', ButtonGroup)
 
 new Vue({
