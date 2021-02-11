da3d22904ecb1036909506ab131c51a8b0c44101
wuyl <2331336537@qq.com>
2021-02-11 14:11:46 +0800

    feat: 添加pager组件

--- src/demo.vue
+++ src/demo.vue
@@ -1,44 +1,24 @@
 <template>
-  <div>
-    <r-nav :selected.sync="selected" vertical>
-      <r-nav-item name="home">首页</r-nav-item>
-      <r-sub-nav name="about">
-        <template slot="title">关于</template>
-        <r-nav-item name="culture">企业文化</r-nav-item>
-        <r-nav-item name="developers">开发团队</r-nav-item>
-        <r-sub-nav name="contacts">
-          <template slot="title">联系方式</template>
-          <r-nav-item name="wechat">微信</r-nav-item>
-          <r-nav-item name="qq">QQ</r-nav-item>
-          <r-sub-nav name="phone">
-            <template slot="title">手机</template>
-            <r-nav-item name="cm">移动</r-nav-item>
-            <r-nav-item name="cu">联通</r-nav-item>
-            <r-nav-item name="cn">电信</r-nav-item>
-          </r-sub-nav>
-        </r-sub-nav>
-
-      </r-sub-nav>
-      <r-nav-item name="hire">招聘</r-nav-item>
-    </r-nav>
-    <p>你好，我是中文</p>
+  <div style="margin: 20px;">
+    <r-pager :total-page="10" :current-page.sync="currentPage"></r-pager>
   </div>
 </template>
 <script>
-import RNav from './nav/nav.vue'
-import RNavItem from './nav/nav-item.vue'
-import RSubNav from './nav/sub-nav.vue'
+import RPager from './pager'
 
 export default {
   name: 'demo',
-  components: { RNav, RNavItem, RSubNav },
+  components: { RPager },
   data () {
     return {
-      selected: 'home'
+      currentPage: 1
     }
   }
 }
 </script>
 <style>
   * {margin: 0; padding: 0; box-sizing: border-box;}
+  body {background: white;}
+</style>
+<style scoped lang="scss">
 </style>
